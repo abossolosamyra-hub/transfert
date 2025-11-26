68e4bf7a6b0cd53e39ae27f1df0d5a5f4aa6cd20
abossolosamyra-hub <abossolosamyra@gmail.com>
2025-11-26 13:59:42 +0100

    modifier code

--- admin.php
+++ admin.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=d
+    evice-width, initial-scale=1.0">
     <link rel="stylesheet" href="style-admin.css" class="rel">
     <title>Mon Dashboard</title>
 </head>
---
+++ poo.java
@@ -0,0 +1,37 @@
+class Contact{
+   string nom;
+   string prenom;
+   int  numerotelephone;
+   string email;
+
+   void Contact(){
+    this.nom;
+    this.prenom;
+    this.numerotelephone;
+    this.emeil;
+   }
+   void Contact(string n,string p, int t, string e){
+   this.nom=n;
+   this.prenom=p;
+   this.numerotelephone=t;
+   this.emeil=e;
+   }
+   void afficherContact(){
+    system.out.println( nom prenom , numerotelephone, email)
+
+    voidmodifiernumerotelephone(int nouveaunumerotelephone)
+    numerotelephone = nouveau numerotelephone;
+   }
+    voidmodifieremail(string nouvelemail){
+    email = nouvelemail;
+    }
+
+}
+Public static void main(string[]arg){
+    Contact c1=new Contact();
+    Contact c2=new Contact("pierre","jean" 652148795,"jean@gmail.com")
+    c1.afficherContact();
+    c2.afficherContact();
+    c1.modifiernumerotelephone(625984132);
+    c2.modifieremail(pierre@gmail.com);293-+
+}
